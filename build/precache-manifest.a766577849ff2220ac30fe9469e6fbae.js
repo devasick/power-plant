0fdfe9c619d3d50fd040c410adac473543933174
Asick Ahamed <ashiq.it@gmail.com>
2020-01-14 14:40:16 +0100

    Power plants live files

---
+++ build/precache-manifest.a766577849ff2220ac30fe9469e6fbae.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf18c1f3d1f8b69e444e0fdfd49d4628",
+    "url": "/power-plants/index.html"
+  },
+  {
+    "revision": "37b5802bab68c50cea10",
+    "url": "/power-plants/static/css/main.99024bbc.chunk.css"
+  },
+  {
+    "revision": "1a36ee91a812ddf6a1e3",
+    "url": "/power-plants/static/js/2.f40a619c.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/power-plants/static/js/2.f40a619c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "37b5802bab68c50cea10",
+    "url": "/power-plants/static/js/main.bbb73c23.chunk.js"
+  },
+  {
+    "revision": "dcb550ccbad39d2419a3",
+    "url": "/power-plants/static/js/runtime-main.aa58acaa.js"
+  },
+  {
+    "revision": "59ff30833a0c765353e9db30ba04aef1",
+    "url": "/power-plants/static/media/logo.59ff3083.svg"
+  }
+]);
